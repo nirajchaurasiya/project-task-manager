5aa555764165a2a442d256797189b416ab9ec8a3
frequel <frequel45@gmail.com>
2024-06-29 13:20:24 +0545

    changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,6 @@ const allowedOrigins = [
    process.env.DEVELOPMENT_CORS_ORIGIN,
    process.env.PRODUCTION_CORS_ORIGIN,
 ];
-console.log(allowedOrigins);
 app.use(
    cors({
       origin: function (origin, callback) {
